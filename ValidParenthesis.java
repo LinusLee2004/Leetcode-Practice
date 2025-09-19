33fa07d2dbcf7119b48de66ab33e1c2be8bc9c3b
LinusLee2004 <linuslee2123@gmail.com>
2025-09-19 13:16:40 -0500

    Fixed issue with checking s vs stack is empty

--- ValidParenthesis.java
+++ ValidParenthesis.java
@@ -1,13 +1,14 @@
 import java.util.Stack;
 
 public class ValidParenthesis {
+
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
             } else {
-                if (s.isEmpty())
+                if (stack.isEmpty())
                     return false;
 
                 char top = stack.pop();
