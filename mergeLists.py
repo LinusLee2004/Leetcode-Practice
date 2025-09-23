3756118e8566b4ee458d5fc8214744bad6ba8d27
LinusLee2004 <linuslee2123@gmail.com>
2025-09-23 09:09:06 -0500

    Practiced writing method again

--- mergeLists.py
+++ mergeLists.py
@@ -16,3 +16,41 @@ class mergeLists():
         else:
             list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
+
+
+    def mergeLists(self, list1, list2):
+        current = head = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        current.next = list1 or list2
+        return head.next
+
+# Helper functions
+def create_linked_list(arr):
+    dummy = current = ListNode()
+    for val in arr:
+        current.next = ListNode(val)
+        current = current.next
+    return dummy.next
+
+def print_linked_list(node):
+    res = []
+    while node:
+        res.append(node.val)
+        node = node.next
+    print(res)
+
+# Test code (runs immediately when script is executed)
+list1 = create_linked_list([2, 5, 5, 5])
+list2 = create_linked_list([1, 7, 8])
+
+solution = mergeLists()
+merged = solution.mergeLists(list1, list2)
+print_linked_list(merged)  # Output: [1, 1, 2, 3, 4, 4]
+
