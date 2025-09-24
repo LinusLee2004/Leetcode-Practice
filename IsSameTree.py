183955b1c14d32bfb1972e7e46c4f3e8a2d5cf4a
LinusLee2004 <linuslee2123@gmail.com>
2025-09-24 09:12:21 -0500

    created TreeNode class

--- IsSameTree.py
+++ IsSameTree.py
@@ -1,3 +1,11 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class IsSameTree():
     def isSameTree(self, p, q):
+        if p.val != q.val:
+            return False
         
