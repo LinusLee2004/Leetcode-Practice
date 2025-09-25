8e48e925e28924bb6dec8fb3bdd2b8bafd42c551
LinusLee2004 <linuslee2123@gmail.com>
2025-09-25 14:55:40 -0500

    Made reverse array method in python after I practiced it in java

---
+++ reverseArray.py
@@ -0,0 +1,7 @@
+class reverseArray():
+    def reverseArray(self, arr):
+        #start:stop:step notation. Stepping back by one spot
+        #each time and python assumes start is at the end of
+        #array and stop is at beginning since -1 is step.
+        return arr[::-1]
+    
