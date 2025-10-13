2f2ac692602f2b63f3bbacaab19d1586a69ef1fd
LinusLee2004 <linuslee2123@gmail.com>
2025-10-13 11:00:18 -0500

    Created set with all numbers in n + 1 range. Then removed all numbers from the set that were in nums array. Finally returned last variable in the set.

--- MissingNumber.py
+++ MissingNumber.py
@@ -1,3 +1,14 @@
 class MissingNumber():
     def missingNumber(self, nums):
-        
+        n = len(nums)
+        testSet = set()
+        for i in range(n + 1):
+            testSet.add(i)
+        for num in nums:
+            if num in testSet:
+                testSet.remove(num)
+        return testSet.pop()
+mn = MissingNumber()
+arr = [0,2,3,4,1,6,7]
+result = mn.missingNumber(arr)
+print(result)
