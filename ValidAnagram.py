b94dd1158adcc4fddaae9119895806adb316307a
LinusLee2004 <linuslee2123@gmail.com>
2025-10-13 10:45:58 -0500

    Created solution where I sort the word in lists and then compare using for loops

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,3 +1,22 @@
 class ValidAnagram():
     def isAnagram(self, s, t):
-        
+        #Check length first
+        if len(s) != len(t):
+            return False
+        #Turn strings into lists
+        sChars = list(s)
+        tChars = list(t)
+        #Sort lists alphabetically
+        sortedS = sorted(sChars)
+        sortedT = sorted(tChars)
+        #Compare lists
+        for i in range(len(sortedS)):
+            if(sortedS[i] != sortedT[i]):
+                return False
+        return True
+    
+testWord = "hellofhdsjd"
+testCompare = "holelfhdsjd"
+iA = ValidAnagram()
+result = iA.isAnagram(testWord, testCompare)
+print(result)
